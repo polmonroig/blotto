d083f6b942f038da3d3ead30d5bb1794f59b17ff
polmonroig <polmonroig@yahoo.es>
2020-05-17 12:47:38 +0200

    Add state container

--- train.py
+++ train.py
@@ -26,10 +26,25 @@ class State:
     def is_terminal(self):
         return self.available_troops == 0
 
+# MAX_TROOPS == level width
+class StateContainer:
+    def __init__(n_battlefields, default):
+        self.level = 1
+        self.max_level = n_battlefields
+        if is_leaf():
+            self.values = [default] * MAX_TROOPS
+        else:
+            self.values = [StateContainer(n_battlefields - 1, default)] * MAX_TROOPS
+
+    def is_leaf(self):
+        return self.level == self.max_level
+
 class Agent:
-    def __init__(self, n_states, e=0.2):
-        self.value_state = [0] * n_states
+    def __init__(self, n_battlefields, e=0.2):
+        self.value_state = StateContainer(n_battlefields, 0)
+        self.returns = StateContainer(n_battlefields, [])
         self.e_greedy = e
+        self.discount = 0.1
 
     def update(self, state):
         if state.is_terminal():
@@ -46,13 +61,24 @@ class Agent:
                 wins += 1
             elif b1 < b2:
                 wins -= 1
+
         if wins > 0:
             print("Player A wins")
         elif wins < 0:
+            win = -win
             print("Player B wins")
         else:
             print("There was a tie")
-        pass
+        reward_a = win
+        reward_b = -win
+        returns_a = 0
+        returns_b = 0
+        while state_a and state_b:
+            returns_a = reward_a * self.discount + reward_a
+            returns_b = reward_b * self.discount + reward_b
+
+            state_a = state_a.parent
+            state_b = state_b.parent
 
 
 n_episodes = int(input("Enter number of episodes "))
