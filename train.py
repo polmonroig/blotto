508dfa2bf9d692873612e44f7af154aae16d0348
polmonroig <polmonroig@yahoo.es>
2020-05-17 12:18:10 +0200

    Change from numpy to simple list

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 # Blotto game is a simple game where you assing
 # troops to battlefields
-from random import random
+from random import random, randint
 import numpy as np
+import copy
 
 # global constants
 MAX_TROOPS = 100
@@ -12,35 +13,39 @@ MAX_TROOPS = 100
 class State:
     def __init__(self, size, available_troops=MAX_TROOPS):
         self.parent = None
-        self.battlefields = np.array(size)
+        self.battlefields = [0] * size
         self.available_troops = available_troops
 
-    def apply_action(self, action):
-        self.parent = self.copy()
+    def apply_action(self, troops):
+        self.parent = copy.copy(self)
         for battlefield in self.battlefields:
             if battlefield == 0:
-                battlefield = action.troops
+                battlefield = troops
                 break
-        self.available_troops -= action.troops
+        self.available_troops -= troops
 
     def is_terminal(self):
         return self.available_troops == 0
 
 class Agent:
     def __init__(self, n_states, e=0.2):
-        self.value_state = np.array(n_states)
+        self.value_state = [0] * n_states
         self.e_greedy = e
 
     def update(self, state):
         if state.is_terminal():
             return False
+        troops = randint(1, state.available_troops)
+        state.apply_action(troops)
+
+        return True
 
     def update_values(self, state_a, state_b):
-        pass 
+        pass
 
 
-n_episodes = input("Enter number of episodes")
-n_battlefields = input("Enter number of battlefields")
+n_episodes = int(input("Enter number of episodes "))
+n_battlefields = int(input("Enter number of battlefields "))
 agent = Agent(n_battlefields)
 for episode in range(n_episodes):
     print("Episode:", episode)
@@ -49,6 +54,8 @@ for episode in range(n_episodes):
     initial_state_b = State(n_battlefields)
     while agent.update(initial_state_a):
         pass
+    print("Battlefields A:", initial_state_a.battlefields)
     while agent.update(initial_state_b):
         pass
+    print("Battlefields B:", initial_state_b.battlefields)
     agent.update_values(initial_state_a, initial_state_b)
