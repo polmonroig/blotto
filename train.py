9c6b7f72494d87053ba78f2ab703f6fbcf635265
polmonroig <polmonroig@yahoo.es>
2020-05-17 11:27:06 +0200

    Add agent

--- train.py
+++ train.py
@@ -7,18 +7,35 @@ import numpy as np
 MAX_TROOPS = 100
 
 # define actions
-actions = Enum("Resign", "AssignTroops")
+ActionType = Enum("ActionType", "Resign AssignTroops")
 
 class Action:
     def __init__(self, action_type):
         self.type = action_type
+        self.troops = 0
+        self.battlefield = 0
+
+    def add_troops(self, battlefield, troops):
+        self.troops = troops
+        self.battlefield = battlefield
 
 class State:
     def __init__(self, size):
         self.battlefields = np.array(size)
+        self.terminal = False
 
     def apply_action(self, action):
+        if action.type == Actions.Resign:
+            self.terminal = True
+        else:
+            self.battlefields[action.battlefield] = action.troops
 
+    def is_terminal(self):
+        return self.terminal
+
+class Agent:
+    def __init__(self, n_states):
+        self.policy = np.array(n_states)
 
 n_battlefields = input("Enter number of #battlefields")
 
